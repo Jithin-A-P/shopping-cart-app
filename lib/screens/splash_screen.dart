7ff502dacb0883c21fb9d2110de3542cf40c8f91
Jithin A P <jithinap009@gmail.com>
2023-02-20 19:40:44 +0530

    Initial setup: Spalsh Screen and Home Screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:shopping_cart/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+      const Duration(seconds: 3),
+      () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text('Splash Screen'),
+      ),
+    );
+  }
+}
